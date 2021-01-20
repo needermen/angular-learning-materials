0511c20547992f8fb7a85cd9e5cea94d039360a2
tschikovani <tsotnechikovani@gmail.com>
2021-01-20 16:32:08 +0400

    feat(layout) - add form navigation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LayoutComponent } from './layout/layout.component';
@@ -10,7 +11,8 @@ import { LayoutComponent } from './layout/layout.component';
     LayoutComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
